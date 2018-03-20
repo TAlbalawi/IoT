3760cd367524c99d31db4bc2577a7ac7473ba604
TAlbalawi <TAlbalawi@users.noreply.github.com>
2018-03-19 21:33:46 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-(function(global, undefined) {
-  'use strict';
-
-  var SimpleDrawingBoard = global.SimpleDrawingBoard;
-
-  var c = document.getElementById('c');
-  global.sdb = new SimpleDrawingBoard(c);
-
-}(window));
